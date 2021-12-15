0edb8cbadca3dc77b4b2470bdf28662a0fe3617b
Mitch Hartigan <mitch.hartigan@gmail.com>
2021-12-14 22:53:41 -0800

    feat: swapped to new Acronym database

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const _loadCollection = async () => {
   try {
     const client = new MongoClient(dbUrl);
     await client.connect();
-    let collection = client.db("mortgagebanking").collection("acronymsv3");
+    let collection = client.db("mortgagebanking").collection("acronymsv4");
 
     return collection;
   } catch (err) {
